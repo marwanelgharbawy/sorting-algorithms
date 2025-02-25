dcb8ad91794962aafcb739d36ff9d70e78e583b0
Marwan <marwanelgharbawy2@gmail.com>
2025-02-26 00:20:54 +0200

    Remove optimization

--- sorting.py
+++ sorting.py
@@ -1,12 +1,8 @@
 def bubble_sort(array):
     for i in range(len(array)):
-        # sorted = True
         for j in range(len(array) - i - 1):
             if array[j] > array[j + 1]:
-                # sorted = False
                 array[j], array[j + 1] = array[j + 1], array[j]
-        # if not sorted:
-        #     break
     
     return array
 
