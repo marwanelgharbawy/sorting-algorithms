6c6f4eb048855a75d4ff391734897c5cc8dcc365
Marwan <marwanelgharbawy2@gmail.com>
2025-02-26 14:56:14 +0200

    add insertion sort

--- sorting.py
+++ sorting.py
@@ -14,8 +14,19 @@ def selection_sort(array):
                 min = j 
         array[i], array[min] = array[min], array[i] # For each pass, smallest element will be at the start
     return array
-    
 
+def insertion_sort(array):
+    for i in range(1, len(array)):  # Assuming first element is sorted
+        key = array[i]
+        j = i - 1
+        while j >= 0 and array[j] > key:  # array[i] is the element to be inserted
+            array[j + 1] = array[j]
+            j -= 1
+        array[j + 1] = key
+
+    return array
+    
+    
 # TODO: Generate random array
 array = [6, 4, 33, 54, 20, 10, 1, 50, 5, 70, 3, 2]
 
@@ -26,3 +37,6 @@ print(array_bubble)
 
 array_select = selection_sort(array)
 print(array_select)
+
+array_insert = insertion_sort(array)
+print(array_insert)
